68fde260a849d7340549b97eac261cddab992503
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-30 00:24:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
       new_arr.each_with_index do |element, index|
         new_arr[index] = element.capitalize
       end
-      puts "#{new_arr.join(" ")}:"
+      puts "  #{new_arr.join(" ")}: #{value.to_s}"
       puts value.to_s
     end
     
